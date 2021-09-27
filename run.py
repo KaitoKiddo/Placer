1d71bcc129942a14ac505f71887b83fcf28a03fa
KaitoKiddo <2603194904@qq.com>
2021-09-27 09:22:14 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+from env import Env
+
+env = Env()
+
+s = env.reset()
+print(s)
+r = 0
+for i in range(5):
+    
+    a = 0
+    s_, reward = env.step(s, a)
+    s = s_
+    r += reward
+
+print(r)
