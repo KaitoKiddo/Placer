9a736480b951042c4e3352ae13eb303281fc1ea8
KaitoKiddo <2603194904@qq.com>
2021-10-14 13:03:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
+from networkx.algorithms import dominance
 from graph_embedding import graph_embedding
-from networkx.readwrite import graph6
 from env import Env
 
 env = Env()
 
 s = env.reset()
-G = graph_embedding(env)
+G = graph_embedding(env, s)
 r = 0
 for i in range(10):
     
     a = 0
-    s_, reward = env.step(s, a)
+    s_, reward, done = env.step(s, a)
     s = s_
     r += reward
 
